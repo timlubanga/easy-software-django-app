5632e39a2dc4b9c1a469d04bf5f1d3107af25dcf
timlubanga <timlubanga@gmail.com>
2021-05-05 01:53:12 +0300

    chore:set up the API documentation using swagger-UI

--- easysoftwaredjangoapp/settings.py
+++ easysoftwaredjangoapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'userAuth',
+    'corsheaders'
     'drf_yasg',
 
 ]
@@ -59,6 +60,7 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
