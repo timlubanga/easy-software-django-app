e46a8959934ff432c01113b9c5390dd461f46a87
timlubanga <timlubanga@gmail.com>
2021-05-05 01:13:18 +0300

    feat:create custom user auth model

---
+++ userAuth/models.py
@@ -0,0 +1,55 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin)
+Create your models here.
+
+
+class UserAccountManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise ValueError(' a user must have a username')
+        if email is None:
+            raise ValueError('a user must have an email address')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if password is None:
+            raise ValueError('Password should not be none')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=300, unique=True, db_index=True)
+    email = models.EmailField(max_length=300, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
